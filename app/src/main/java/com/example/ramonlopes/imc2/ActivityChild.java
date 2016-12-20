e193a9864573915b8f0fa948175674d5ebd1d78e
out_fourtime_hugo <rafael@fourtime.com>
2016-12-20 00:05:38 -0200

    Atualização

--- app/src/main/java/com/example/ramonlopes/imc2/ActivityChild.java
+++ app/src/main/java/com/example/ramonlopes/imc2/ActivityChild.java
@@ -5,13 +5,41 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ActivityChild extends AppCompatActivity {
 
+    Button btn;
+    EditText edtPeso,edtAltura;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+
+        btn = (Button)findViewById(R.id.submit);
+        edtAltura = (EditText) findViewById(R.id.edt1);
+        edtPeso = (EditText) findViewById(R.id.edt2);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (edtAltura.getText().toString().equals("")) {
+                    Toast.makeText(getApplicationContext(), "Campo Altura está vazio!", Toast.LENGTH_SHORT).show();
+                    edtAltura.requestFocus();
+                } else if (edtPeso.getText().toString().equals("")) {
+                    Toast.makeText(getApplicationContext(), "Campo Altura está vazio!", Toast.LENGTH_SHORT).show();
+                    edtPeso.requestFocus();
+                } else {
+                }
+            }
+        });
+
+
     }
 
 
